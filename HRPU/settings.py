6fae9a85dffe9d5414bc18c80652b7010314c15e
YoungKealohi <kyoung23@my.hpu.edu>
2025-11-16 23:47:52 -1000

    going back to postgres

--- HRPU/settings.py
+++ HRPU/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'HRPU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hrpudb',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
