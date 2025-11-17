0a80bba8b8dd3388791f167480f6f29a75ad2f98
YoungKealohi <kyoung23@my.hpu.edu>
2025-11-17 00:44:23 -1000

    back to sqlite3 bc at least it works locally

--- HRPU/settings.py
+++ HRPU/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'HRPU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hrpudb',
-        'USER': 'admin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
